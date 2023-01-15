bb5a10d2ef34a82241ea65d14262c783f3221202
Armando2396 <armandomartinezbatista2@gmail.com>
2023-01-15 16:39:59 -0500

    agregando ejercicio1

---
+++ Ejercicio1/src/com/ejercicio/TiposDeDatos.java
@@ -0,0 +1,45 @@
+package com.ejercicio;
+
+public class TiposDeDatos {
+    public static void main(String[] args) {
+
+        // numericos
+        byte  variable1 = 1;
+        short variable2 = 10;
+        int variable3 = 15;
+        long variable4 = 100;
+
+        // decimales
+
+        float variable5 = 5.5f;
+        double variable6 = 10.5d;
+
+        //booleanos
+
+        boolean variable7 = false;
+        boolean variable8 = true;
+
+        //texto
+
+        char variable9 = 'a';
+        String variable10 = "lorem ipsum dolor sit amet ....";
+
+        // imprimiendo los tipos de datos visto en el curso
+
+        System.out.println("byte "+ variable1);
+        System.out.println("short "+ variable2);
+        System.out.println("int "+ variable3);
+        System.out.println("long "+ variable4);
+        System.out.println("float "+ variable5);
+        System.out.println("double "+ variable6);
+        System.out.println("boolean "+ variable7);
+        System.out.println("boolean "+ variable8);
+        System.out.println("char "+ variable9);
+        System.out.println("String "+ variable10);
+
+
+
+
+
+    }
+}
